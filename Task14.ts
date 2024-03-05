b0943800e6006e84c95f26392ab66f46f7aeba5b
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-03-05 10:58:44 +0500

    Create Task14.ts

---
+++ Task14.ts
@@ -0,0 +1,11 @@
+// #14: Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+let relatives = [
+    'Hassan',
+    'Shees',
+    'Fatima',
+]
+
+for (let relative of relatives){
+    console.log('Hi, '+relative+" You are invited to today's Dinner at my Home!")
+}
