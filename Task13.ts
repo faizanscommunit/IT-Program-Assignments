ee2819a84c072d0dbb1eea578b459d18605e3d85
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-03-05 10:57:04 +0500

    Create Task13.ts

---
+++ Task13.ts
@@ -0,0 +1,11 @@
+// # 13: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+let favourit_mode = [
+    'Car',
+    'Electric Bus',
+    'bike',
+]
+
+for (let name of favourit_mode){
+    console.log('I would like to own a '+name+'.')
+}
