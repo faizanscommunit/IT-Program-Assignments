196b3f7c42f8da2b19fe679e8a97ad44bba6a5a7
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-03-05 10:40:15 +0500

    Delete Task02.js

--- Task02.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// #02: Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-Object.defineProperty(exports, "__esModule", { value: true });
-var user = 'Muhammad Faizan';
-console.log("Hello ".concat(user, ", would you like to learn some Python today?"));
