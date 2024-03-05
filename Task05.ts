7e44410ea3163a8a2e3d57b5dd69d47e45346743
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-03-05 10:48:49 +0500

    Create Task05.ts
    // #05: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.

---
+++ Task05.ts
@@ -0,0 +1,6 @@
+// #05: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person:string = "Quaid e Azam"
+console.log(`${famous_person} once said, “I do not believe in taking the right decision, I take a decision and make it right.”`)
+
+export{}
