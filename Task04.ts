9d47cb14313667319a3bb3fd49c174cf64d41b7f
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-03-05 10:45:53 +0500

    Create Task04.ts
    // #04: Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
    // Albert Einstein once said, “A person who never made a mistake never tried anything new.”

---
+++ Task04.ts
@@ -0,0 +1,6 @@
+// #04: Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+console.log('Quaid e Azam once said, “I do not believe in taking the right decision, I take a decision and make it right.”')
+
+export{}
