0e94809dc30539afcead4240270566d11e632ee4
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-03-05 10:42:55 +0500

    Add files via upload
    // #03: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.

---
+++ Task03.ts
@@ -0,0 +1,8 @@
+// #03: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let user = 'Ahmed'
+console.log(user.toLowerCase()) 
+console.log(user.toUpperCase())
+console.log(user.charAt(0).toUpperCase()+ user.slice(1).toLowerCase())
+
+export{}
