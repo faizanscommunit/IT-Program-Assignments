6c6da29642abd64c92d972db829fae21832868a3
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-03-05 10:52:22 +0500

    Create Task09.ts

---
+++ Task09.ts
@@ -0,0 +1,6 @@
+
+// #09: Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message
+
+let favorite_number = 7
+let msg = `My favourite number is:  ${favorite_number}`
+console.log(msg)
