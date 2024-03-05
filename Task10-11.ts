831c97c1aead83354ea48832a3a2814a8c0f3de5
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-03-05 10:55:07 +0500

    Create Task10-11.ts

---
+++ Task10-11.ts
@@ -0,0 +1,12 @@
+// #10:  Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// #11: Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time 
+
+const friends = ['Shaheer', 'Fahad', 'Rafay', 'Yousha']
+for (let friend of friends){
+    console.log(friend)
+}
+
+
+
+export{}
