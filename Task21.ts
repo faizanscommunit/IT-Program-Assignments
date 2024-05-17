a4d7f946a82f8fb00e0f06f6f4a540c4e671c14c
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-05-17 11:20:29 +0500

    Create Task21.ts

---
+++ Task21.ts
@@ -0,0 +1,8 @@
+// # 21 Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let user: {name: string, institute: string, age: number, gender: 'Male' | 'Female'} = {
+    name: 'Muhammad Faizan',
+    institute: 'GIAIC',
+    age: 17,
+    gender: 'Male',
+}
+console.log(user)
