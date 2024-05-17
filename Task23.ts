c2dd94cf2a42c1383c735c834ede549dca31f03c
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-05-17 11:36:27 +0500

    Create Task23.ts

---
+++ Task23.ts
@@ -0,0 +1,52 @@
+//  #23 Conditional Tests: Write a series of conditional tests. Predict the results of each test.
+// Test 01
+let food = 'biryani'
+console.log("Is food == biryani or not? 'I think YES!' ")
+console.log(food == 'biryani')
+console.log(`It returns ${food == 'biryani'} which means food is biryani!`)
+// Test 02
+let car = 'corolla'
+console.log("Is car == corolla or not? 'I think YES!' ")
+console.log(car == 'corolla')
+console.log(`It returns ${car == 'corolla'} which means car is corolla!`)
+// Test 03
+let laptop = 'Dell'
+console.log("Is laptop == Dell or not? 'I think YES!' ")
+console.log(laptop == 'Dell')
+console.log(`It returns ${laptop == 'Dell'} which means laptop is Dell!`)
+// Test 04
+let mobile = 'oppo'
+console.log("Is mobile == oppo or not? 'I think YES!' ")
+console.log(mobile == 'oppo')
+console.log(`It returns ${mobile == 'oppo'} which means mobile is oppo!`)
+// Test 05
+let pen = 'piano'
+console.log("Is pen == piano or not? 'I think YES!' ")
+console.log(pen == 'piano')
+console.log(`It returns ${pen == 'piano'} which means pen is piano!`)
+// Test 06
+let dinner = 'pasta'
+console.log("Is dinner == biryani or not? 'I think NO!' ")
+console.log(dinner == 'biryani')
+console.log(`It returns ${dinner == 'biryani'} which means dinner is not biryani!`)
+// Test 07
+let bike = 'unique'
+console.log("Is bike == Crown or not? 'I think NO!' ")
+console.log(bike == 'Crown')
+console.log(`It returns ${bike == 'Crown'} which means bike is not Crown!`)
+// Test 08
+let mouse = 'Dell'
+console.log("Is mouse == hp or not? 'I think NO!'")
+console.log(mouse == 'hp')
+console.log(`It returns ${mouse == 'hp'} which means mouse is not of hp!`)
+// Test 09
+let fruit = 'apple'
+console.log("Is fruit == Mango or not? 'I think NO!' ")
+console.log(fruit == 'Mango')
+console.log(`It returns ${fruit == 'Mango'} which means fruit is not Mango!`)
+// Test 10
+let institute = 'GIAIC'
+console.log("Is institute == PIAIC or not? 'I think NO!' ")
+console.log(institute == 'PIAIC')
+console.log(`It returns ${institute == 'PIAIC'} which means institute is not PIAIC!`)
+
