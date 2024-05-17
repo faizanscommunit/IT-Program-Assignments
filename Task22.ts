b9877f551fc4ad8c9bbaf6512f9927f7e4a7264e
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-05-17 11:24:26 +0500

    Create Task22.ts

---
+++ Task22.ts
@@ -0,0 +1,4 @@
+// # 22 Intentional Error: Try to produce an array index error in one of your programs. Correct the error before finishing.
+let rivers = ['Jehlum','Ravi']
+console.log(rivers[2]) // Asking for out of index value, which will return UNDEFINED
+console.log(rivers[0]) // Now asking for correct index value
