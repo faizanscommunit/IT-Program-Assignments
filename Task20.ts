9b6a3a510bf61aa481118472979cb7276bdcb4ba
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-05-17 10:57:20 +0500

    Create Task20.ts

---
+++ Task20.ts
@@ -0,0 +1,3 @@
+// # 20
+let beautiful_places = ['Mount Everest','K2', 'Murree', 'River Jehlum', 'Islamabad', 'Gilgit']
+console.log('Some beautiful places to visit in Pakistan are:',beautiful_places)
