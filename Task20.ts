fc88fac55bcba2f0a1ae06a30109a14edf0aea30
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-05-17 11:01:17 +0500

    Update Task20.ts

--- Task20.ts
+++ Task20.ts
@@ -1,3 +1,4 @@
-// # 20
-let beautiful_places = ['Mount Everest','K2', 'Murree', 'River Jehlum', 'Islamabad', 'Gilgit']
-console.log('Some beautiful places to visit in Pakistan are:',beautiful_places)
+// # 20 Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let beautiful_places:string[] = ['Mount Everest','K2', 'Murree', 'River Jehlum', 'Islamabad', 'Gilgit']
+console.log('Some beautiful places to visit in Pakistan are: ',beautiful_places)
