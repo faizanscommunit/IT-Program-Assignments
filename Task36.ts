56c51e70240765c46c17c3a0f7364e6be30f036a
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-05-24 10:58:54 +0500

    Create Task36.ts

---
+++ Task36.ts
@@ -0,0 +1,7 @@
+
+// # 36 T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+function make_shirt(size:string, msg:string){
+    console.log(`I have a ${size} shirt with ${msg} printed on it.`);
+}
+make_shirt('small', 'Faizanscommunit')
