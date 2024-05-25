71834bbf58bef358f9d053ea6deb1909c12ac7b1
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-05-25 13:18:28 +0500

    Update Task16.ts

--- Task16.ts
+++ Task16.ts
@@ -1,26 +1,31 @@
 // #16: More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
 // • Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
-
 let relatives = [
     'Hassan',
     'Shees',
     'Fatima',
 ]
+
 for (let relative of relatives){
     console.log(`Hi, ${relative} You are invited to today's Dinner at my Home!`)
 }
-console.log(`${relatives[2]}, can't come to dinner!`)
-relatives[2] = 'Muhammad Rayyan'
+// • Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+let cantCome = 'Hassan';
+console.log(`${cantCome}, can't come to dinner!`)
+// • Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+let newGuest:string = 'Muhammad Rayyan'
+relatives[relatives.indexOf(cantCome)] = newGuest;
+// • Print a second set of invitation messages, one for each person who is still in your list.
 for (let name of relatives){
-    console.log('Hi, '+name+" You are invited to today's Dinner at my Home!")
+    console.log(`Hi ${name}, You are invited to today's Dinner at my Home!`)
 }
+console.log(relatives);
+
 console.log('I got a big table, so get ready for big dinner party!')
-
-
 // • Add one new guest to the beginning of your array.
 relatives.unshift('Amir Saleem') // Start
 // • Add one new guest to the middle of your array. 
-relatives.splice(2,0,'Saleem') // Middle
+relatives.splice(relatives.length/2,0,'Saleem') // Middle
 // • Use append() to add one new guest to the end of your list. 
 relatives.push('Muhammad Nouman') // End
 // • Print a new set of invitation messages, one for each person in your list.
@@ -28,4 +33,3 @@ for (let name of relatives){
     console.log(`Hi, ${name} You are invited to today's Dinner at my Home!`)
 }
 
-export{}
