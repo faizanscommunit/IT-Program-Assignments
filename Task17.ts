b9dd1f14d623bc39ff3e477898806146cd066e6a
Faizanscommunit <58328738+faizanscommunit@users.noreply.github.com>
2024-05-25 13:24:16 +0500

    Update Task17.ts

--- Task17.ts
+++ Task17.ts
@@ -1,34 +1,38 @@
 // #17: Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
 // • Start with your program from Exercise 16. Add a new line that prints a message saying that you can invite only two people for dinner.
 
-
 let relatives = [
     'Hassan',
     'Shees',
     'Fatima',
 ]
+
 for (let relative of relatives){
     console.log(`Hi, ${relative} You are invited to today's Dinner at my Home!`)
 }
-console.log(`${relatives[2]}, can't come to dinner!`)
-relatives[2] = 'Muhammad Rayyan'
+// • Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+let cantCome = 'Hassan';
+console.log(`${cantCome}, can't come to dinner!`)
+// • Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+let newGuest:string = 'Muhammad Rayyan'
+relatives[relatives.indexOf(cantCome)] = newGuest;
+// • Print a second set of invitation messages, one for each person who is still in your list.
 for (let name of relatives){
-    console.log('Hi, '+name+" You are invited to today's Dinner at my Home!")
+    console.log(`Hi ${name}, You are invited to today's Dinner at my Home!`)
 }
+console.log(relatives);
+
 console.log('I got a big table, so get ready for big dinner party!')
-
-
 // • Add one new guest to the beginning of your array.
 relatives.unshift('Amir Saleem') // Start
 // • Add one new guest to the middle of your array. 
-relatives.splice(2,0,'Saleem') // Middle
+relatives.splice(relatives.length/2,0,'Saleem') // Middle
 // • Use append() to add one new guest to the end of your list. 
 relatives.push('Muhammad Nouman') // End
 // • Print a new set of invitation messages, one for each person in your list.
 for (let name of relatives){
     console.log(`Hi, ${name} You are invited to today's Dinner at my Home!`)
 }
-
 console.log('I can invite only two people for dinner now !')
 
 
@@ -36,7 +40,7 @@ console.log('I can invite only two people for dinner now !')
 while (relatives.length > 2){
     let removed_guest = relatives.pop()
     if (removed_guest){
-        console.log(removed_guest+" Sorry I can't invite you due to unavailabity of table!")
+        console.log(removed_guest+", Sorry I can't invite you due to unavailabity of table!")
     }
 }
 
@@ -47,9 +51,10 @@ for (let name of relatives){
 }
 
 // • Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
-
-while (relatives.length = 0){
-    relatives.pop()
-}
+// ---> Method 01 (TO REMOVE REMAINING ELEMENTS)
+relatives.splice(0, relatives.length) 
+// ---> Method 02 (TO REMOVE REMAINING ELEMENTS)
+// while (relatives.length = 0){
+//     relatives.pop()
+// }
 console.log(relatives)
-export{}
